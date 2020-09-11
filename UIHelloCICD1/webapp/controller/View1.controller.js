66d732405cfe942994056186272dd3632f7c4c44
Sooyeon You <syyou@hyundai.com>
2020-09-11 06:08:43 +0000

    init

---
+++ UIHelloCICD1/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("hellocicd1.UIHelloCICD1.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ UIHelloCICD1/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,12 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require([
+		"hellocicd1/UIHelloCICD1/test/integration/AllJourneys"
+	], function () {
+		QUnit.start();
+	});
+});
